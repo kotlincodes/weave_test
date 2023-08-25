1a2d117868b7f74f610539a343e3048092cddecc
Sayandh KP <sayandhkp59@gmail.com>
2023-08-26 01:42:25 +0530

    Native iOS UI basic added

--- ios/Runner/FLPlugin.swift
+++ ios/Runner/FLPlugin.swift
@@ -1,8 +1,9 @@
-//
-//  FLPlugin.swift
-//  Runner
-//
-//  Created by Sayandh on 26/08/23.
-//
+import Flutter
+import UIKit
 
-import Foundation
+class FLPlugin: NSObject, FlutterPlugin {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let factory = FLNativeViewFactory(messenger: registrar.messenger())
+        registrar.register(factory, withId: "<platform-view-type>")
+    }
+}
